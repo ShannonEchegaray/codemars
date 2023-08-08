eac6d94889ab27627e1243c581bed3e966bb8ba1
linuxmobile <bdiez19@gmail.com>
2023-08-08 16:32:12 -0300

    Feat 🌈: added brand colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,15 @@
-import {
-  defineConfig,
-  presetAttributify,
-  presetWind,
-} from 'unocss'
+import { defineConfig, presetAttributify, presetWind } from "unocss";
 
 export default defineConfig({
-  presets: [
-    presetAttributify(),
-    presetWind(),
-  ],
-})
+	presets: [presetAttributify(), presetWind()],
+	theme: {
+		colors: {
+			"brand-dark": "#08070C",
+			"brand-light": "#F9F9F6",
+			"brand-red": "#A80202",
+			"brand-orange": "#F0641A",
+			"brand-green": "#71756F",
+			"brand-gray": "#E0D7BC",
+		},
+	},
+});
